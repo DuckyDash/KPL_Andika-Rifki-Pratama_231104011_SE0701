39cab2ae02c2e57845b99b0022c1de2a3c7467e0
DuckyDash <andikasutarni@gmail.com>
2025-03-15 18:32:18 +0700

    menambahkan implementasi dengan table-driven

---
+++ 04_OOP/TPMOD3_2311104011/TableDriven/KodePos.js
@@ -0,0 +1,25 @@
+class KodePos {
+  constructor() {
+    this.data = {
+      "Batununggal": 40266,
+      "Kujangsari": 40287,
+      "Mengger": 40267,
+      "Wates": 40256,
+      "Cijagra": 40287,
+      "Jatisari": 40286,
+      "Margasari": 40286,
+      "Sekejati": 40286,
+      "Kebonwaru": 40272,
+      "Maleer": 40274,
+      "Samoja": 40273
+    };
+  }
+
+  getKodePos(kelurahan) {
+    return this.data[kelurahan] || "Kode pos tidak tersedia";
+  }
+}
+
+const kodePos = new KodePos();
+console.log(kodePos.getKodePos("Batununggal")); 
+console.log(kodePos.getKodePos("TidakAda"));
---
+++ 04_OOP/management.js
@@ -0,0 +1,24 @@
+class Person{
+    name;
+    age;
+
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    getname(){
+        return this.name;
+    }
+    getFullName(){
+        return this.age;
+    }
+
+}
+
+
+
+const orang = new Person("John Doe", 33);
+console.log(personal.name);
+console.log(personal.age);
+console.log(personal.getName());
+
